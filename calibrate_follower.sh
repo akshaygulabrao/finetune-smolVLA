b43c746304c8c8043235d66477839d6a6b674a27
Akshay Gulabrao <aksgula22@gmail.com>
2025-06-15 11:14:15 -0700

    calibration files for leader and follower

---
+++ calibrate_follower.sh
@@ -0,0 +1,4 @@
+/Users/ox/miniconda3/envs/lerobot/bin/python -m lerobot.calibrate \
+    --robot.type=so101_follower \
+    --robot.port=/dev/tty.usbmodem5A680096771 \
+    --robot.id=follower
--- calibrate_leader.sh
+++ calibrate_leader.sh
@@ -1,1 +1,4 @@
-
+/Users/ox/miniconda3/envs/lerobot/bin/python -m lerobot.calibrate \
+    --teleop.type=so101_leader \
+    --teleop.port=/dev/tty.usbmodem58FA0963261 \
+    --teleop.id=leader
