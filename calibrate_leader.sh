db18d92ce78564ffdf75679abf131e221a88ccde
Akshay Gulabrao <aksgula22@gmail.com>
2025-06-15 11:40:05 -0700

    path params script

--- calibrate_leader.sh
+++ calibrate_leader.sh
@@ -1,4 +1,9 @@
+USER="ox"
+REPO_PATH="/Users/$USER/Documents/hackathon-14jun25/"
+cp /Users/$USER/.cache/huggingface/lerobot/calibration/teleoperators/so101_leader/leader.json 
 /Users/ox/miniconda3/envs/lerobot/bin/python -m lerobot.calibrate \
     --teleop.type=so101_leader \
     --teleop.port=/dev/tty.usbmodem58FA0963261 \
     --teleop.id=leader
+
+cp /Users/$USER/.cache/huggingface/lerobot/calibration/teleoperators/so101_leader/leader.json $REPO_PATH
--- teleoperate.sh
+++ teleoperate.sh
@@ -1,10 +1,14 @@
+USER="ox"
+CALIBRATION_PATH="/Users/${USER}/Documents/hackathon-14jun25/"
 /Users/ox/miniconda3/envs/lerobot/bin/python -m lerobot.teleoperate \
     --teleop.type=so101_leader \
     --teleop.port=/dev/tty.usbmodem58FA0963261 \
     --teleop.id=leader \
+    --teleop.calibration_dir=$CALIBRATION_PATH \
     --robot.type=so101_follower \
     --robot.port=/dev/tty.usbmodem5A680096771 \
     --robot.id=follower \
+    --robot.calibration_dir=$CALIBRATION_PATH \
     --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 1920, height: 1080, fps: 30}}" \
     --display_data=True
 
